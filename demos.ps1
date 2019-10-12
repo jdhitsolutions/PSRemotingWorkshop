dd25b2fa1cc46f04b6b39ce58dc2d6ff7de9f101
Jeff Hicks <jhicks@jdhitsolutions.com>
2019-10-12 08:53:13 -0400

    initial

---
+++ demos.ps1
@@ -0,0 +1,39 @@
+return "This is a guided walkthrough script"
+
+#region PowerShell Remoting Fundamentals
+
+#endregion
+
+#region Configuring SSL
+
+#endregion
+
+#region The Dreaded 2nd Hop
+
+#credssp
+
+#kerberos delegation
+
+#endregion
+
+#region Using disconnected sessions
+
+#endregion
+
+#region Remoting at Scale
+
+
+#endregion
+
+#region Constrained Endpoints
+
+#endregion
+
+#region Getting started with Just Enough Administration
+
+#endregion
+
+#region SSH Remoting with PowerSHell Core/7
+
+#endregion
+
